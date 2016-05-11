12e97c054579d36e4413bcd86689717ac1d1b3df
Alan <chin.alan.q@gmail.com>
2016-05-11 01:25:14 -0500

    added empty robot.py

---
+++ scripts/robot.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# license removed for brevity
+import math
+import rospy
+import random
+
+class Robot():
+    def __init__(self):
+
+
+if __name__ == '__main__':
+    try:
+        Ro = Robot()
+
+    except rospy.ROSInterruptException:
+        pass
