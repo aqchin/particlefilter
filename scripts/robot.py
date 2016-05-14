adccf24e1986df5f2999091dff93340070cacb00
Alan <chin.alan.q@gmail.com>
2016-05-13 23:46:17 -0500

    robot.py

--- scripts/robot.py
+++ scripts/robot.py
@@ -3,9 +3,15 @@
 import math
 import rospy
 import random
+import map_utils
+import helper_functions
 
 class Robot():
     def __init__(self):
+
+        self.config
+        rospy.init_node('Robot')
+
 
 
 if __name__ == '__main__':
