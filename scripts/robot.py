61e4e726aedab3ac6a609a49113ef3b66e332a79
Dhruv Kaushal <dhruvrrp@gmail.com>
2016-05-14 15:35:46 -0500

    Initial move

--- scripts/robot.py
+++ scripts/robot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # license removed for brevity
 import math
+import numpy
 import rospy
 import random
-import map_utils
+from map_utils import Map
 import helper_functions
 from read_config import read_config
 from nav_msgs.msg import OccupancyGrid, MapMetaData
@@ -15,12 +16,19 @@ class Robot():
     def __init__(self):
 
         self.config = read_config()
+        print "STARTUSSSSSSSSS"
         rospy.init_node('Robot')
-
+        print "HAHAHAHAHAHAHAHAHA"
+        self.laserStatus = False
+        self.map_data_bool = True
+        self.rate = rospy.Rate(1)
+        self.rate.sleep()
+        self.move_list = self.config["move_list"]
         self.map_subscriber = rospy.Subscriber(
                 "/map",
                 OccupancyGrid,
-                self.map_data)
+                self.map_data
+        )
 
         self.pose_pub = rospy.Publisher(
                 "/particlecloud",
@@ -39,8 +47,9 @@ class Robot():
                 LaserScan,
                 self.updateLaserData
         )
-
+        print "WTFFFFFFFFFFFFFFF"
         random.seed(100)
+        rospy.spin()
 
     def updateLaserData(self, data):
         if self.laserStatus:
@@ -54,51 +63,67 @@ class Robot():
 
 
     def map_data(self, map_d):
+        print "WTF PLZ" 
+        if self.map_data_bool:
+            self.map_data_bool = False
+            self.map_meta = MapMetaData()
+            map_meta = map_d.info
+            self.width = map_meta.width
+            self.height = map_meta.height
 
-        self.map_meta = MapMetaData()
-        map_meta = map_d.info
-        self.width = map_meta.width
-        self.height = map_meta.height
-        self.data = map_d.data
+            self.data = map_d.data
+            print "MAP DATA"
+            self.pose_array = PoseArray()
+            self.pose_array.header.stamp = rospy.Time.now()
+            self.pose_array.header.frame_id = 'map'
+            self.pose_array.poses = []
+
+
+            self.particles = []
+            for i in range(0, self.config["num_particles"]):
+
+                point = []      
+                point.append(random.randrange(0, self.width))
+                point.append(random.randrange(0, self.height))
+                d_angle = random.randrange(0, 360)
+                point.append(float(d_angle*math.pi/180.0))
+                point.append((1/self.config["num_particles"]))
+
+                self.particles.append(point)
+            print "POSE PRINT PUBLISH"
+
+            self.publish_particles(self.particles)
+
+            self.lmap = Map(map_d) #or map_d.data
+            self.buildLMap()
+            self.initial_move()
+
+            self.make_move()
+
+    def publish_particles(self, particless):
 
         pose_array = PoseArray()
         pose_array.header.stamp = rospy.Time.now()
         pose_array.header.frame_id = 'map'
         pose_array.poses = []
-
-
-        self.particles = []
-        for i in range(0, self.config["num_particles"]):
-
-          
-            x = random.randrange(0, self.width)
-            y = random.randrange(0, self.height)
-            d_angle = random.randrange(0, 360)
-            angle = float(d_angle*math.pi/180.0)
-            weight = (1/self.config["num_particles"])
-            pose = helper_functions.get_pose(x, y, angle)
-
+    
+        for p in particless:
+            pose = helper_functions.get_pose(p[0], p[1], p[2])
             pose_array.poses.append(pose)
-            point = (x,y,angle,weight,pose)
-            self.particles.append(point)
-
+        
         self.pose_pub.publish(pose_array)
         self.rate.sleep()
-
-        self.lmap = Map(map_d) #or map_d.data
-
-        self.initial_move()
 
     def buildLMap(self):
 
         self.occupied = []
         self.all_points = []
 
-        for r in range(len(self.height)):
-            for c in range(len(self.width)):
+        for r in range(self.height):
+            for c in range(self.width):
 
                 (x, y) = self.lmap.cell_position(r, c);
-                if self.data[r][c] == 100:
+                if self.data[r*self.height + c] == 100:
                     self.occupied.append([x, y])
 
                 self.all_points.append([x, y])
@@ -107,7 +132,7 @@ class Robot():
         kdt = KDTree(self.occupied)
         (dist, ind) = kdt.query(self.all_points, k=1)
         self.laser_sd = self.config["laser_sigma_hit"]
-        result_pdf = [self.PDF(temp, self.laser_sd) for temp in kdt]
+        result_pdf = [self.PDF(temp, self.laser_sd) for temp in dist]
 
         self.lmap.grid = numpy.reshape(result_pdf, self.lmap.grid.shape)
         self.lmap_pub.publish(self.lmap.to_message())
@@ -121,39 +146,57 @@ class Robot():
 
         in_move = self.move_list.pop()
         move_angle = in_move[0]
-        move_function(move_angle, 0)
-
+        helper_functions.move_function(move_angle, 0)
+        self.resample()
         for i in self.particles:
             i[2]+=random.gauss(0, self.config["first_move_sigma_angle"])
+           
+        self.publish_particles(self.particles)
+        
 
-        #TO DO PUBLISH PARticles
+        for i in range(1):
+            helper_functions.move_function(0, in_move[1])
+            self.resample()
+            for p in self.particles:
+                a= p[0]
+                b = p[1]
+                p[0]+=p[0]*math.cos(p[2])
+                p[0]+=random.gauss(0, self.config["first_move_sigma_x"])
+                
+                p[1]+=p[1]*math.sin(p[2])
+                p[1]+=random.gauss(0, self.config["first_move_sigma_y"])
+                print "Probbbbbbbbbbbbbbbbbb"
+                print a, " also ", b
+                print p[0], " anda ", p[1]
+                print math.fabs(p[0]+a), " AND ", math.fabs(p[1]+b)
+            self.publish_particles(self.particles)
 
-        for i in range(in_move[2]):
-            move_function(0, in_move[1])
-
-
-
+    def make_move(self):
+        print "HA" 
 
     def resample(self):
 
-        getLaserData()
+        self.getLaserData()
         total_weight = 0
         for p in self.particles:
             x = p[0]
             y = p[1]
-            if map.data[x][y] == 100:
+            (row, col) = self.lmap._cell_index(x,y)
+
+
+            if self.lmap.get_cell(x, y) == 1:
                 p[3] = 0
             p_total = 0
-            for i in range(len(current_scan.ranges)):
-                cur_angle = p.angle + current_scan.angle_min
-                cur_angle += i * current_scan.angle_increment
+            for i in range(len(self.current_scan.ranges)):
+                cur_angle = p[2] + self.current_scan.angle_min
+                cur_angle += i * self.current_scan.angle_increment
                 new_x = x * math.cos(cur_angle)
                 new_y = y + math.sin(cur_angle)
 
                 #Add from likelihood field
                 pz = self.config["laser_z_hit"] * self.lmap.get_cell(new_x,new_y)
                 pz += self.config["laser_z_rand"]
-                p_total += (pz**2)
+                p_total += (pz)
 
             p[3] = p[3] * p_total
 
